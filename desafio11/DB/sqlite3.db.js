702aab15aa3eb75d4122a45d999f283e9aef7b77
JuampiRibero <juampiribero@gmail.com>
2022-06-01 23:50:14 -0300

    Subo al repositorio Desafio 12

--- desafio11/DB/sqlite3.db.js
+++
@@ -1,9 +0,0 @@
-const sqlite3Connect = {
-    client: "sqlite3",
-    connection: {
-        filename: "./DB/chat.sqlite",
-    },
-    useNullAsDefault: true,
-};
-
-module.exports = { sqlite3Connect };
